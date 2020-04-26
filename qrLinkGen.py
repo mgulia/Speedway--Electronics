561b5f015988ba4e09a980e0d4516ec4fb46d0d6
chrislevy14 <chrislevy360@gmail.com>
2020-04-25 22:16:16 -0400

    Update qrLinkGen.py

--- qrLinkGen.py
+++ qrLinkGen.py
@@ -1,15 +1,39 @@
-import os
+"""
+=======================================================================
+* Program Name:   QR Code Link Generator
+* Description:    Using a list of links in a text file, Program outputs QR codes in directory
+* Comments:       Requires list to run (method to create list pending)
+*
+* EPICS SPEEDWAY // ELECTRONICS
+=======================================================================
+"""
+from os import getcwd, mkdir
 import sys
 import qrcode
 
-DataPath = os.getcwd()
+# Global Variables
+DataPath = getcwd()
 linkPath = DataPath + '/qrForLinks'
 
-#Creates folder that will contain QR codes
+"""
+=======================================================================
+* Function Name:  createFolder
+* Description:    Creates a folder that will contain QR Codes
+* Parameters:     void
+* Return:         void
+=======================================================================
+"""
 def createFolder():
-    os.mkdir(linkPath)
+    mkdir(linkPath)
 
-#Loops through textfile and sends data to helper function
+"""
+=======================================================================
+* Function Name:  loopFile
+* Description:    Loops through text file to get link
+* Parameters:     void
+* Return:         void
+=======================================================================
+"""
 def loopFile():
     with open("links.txt", 'r') as FILE:
         dataFile = FILE.readlines()
@@ -17,7 +41,15 @@ def loopFile():
     for idx, line in enumerate(dataFile):
         generator(idx+1, line)
 
-#Helper function that generates QR codes
+"""
+=======================================================================
+* Function Name:  generator
+* Description:    Helper function that generates the codes
+* Parameters:     count, int, QR code number 
+*                 link, string, Link data to be transformed to QR code
+* Return:         void
+=======================================================================
+"""
 def generator(count, link):
     qr = qrcode.QRCode()
     qr.add_data(link)
@@ -25,7 +57,11 @@ def generator(count, link):
     img = qr.make_image()
     img.save(linkPath + '/qrCode_' + str(count) + '.png') #saves QR codes
 
-
+"""
+*****************************
+MAIN
+*****************************
+"""
 if __name__ == "__main__":
     createFolder()
     loopFile()
